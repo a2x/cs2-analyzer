3d4ef272194b181227fe326bd47e34021d9efb70
a2x <45197573+a2x@users.noreply.github.com>
2024-10-10 12:18:48 +1000

    Fix broken patterns

--- src/analysis/client/offsets.rs
+++ src/analysis/client/offsets.rs
@@ -12,16 +12,16 @@ static PATTERNS: phf::Map<&'static str, &'static [Atom]> = phf_map! {
     "dwGameEntitySystem" => pattern!("488b1d${'} 48891d"),
     "dwGameEntitySystem_highestEntityIndex" => pattern!("8b81u2?? 8902 488bc2 c3 cccccccc 48895c24? 48896c24"),
     "dwGameRules" => pattern!("48891d${'} ff15${} 84c0"),
-    "dwGlobalVars" => pattern!("48890d${'} 488941"),
+    "dwGlobalVars" => pattern!("488915${'} 488942"),
     "dwGlowManager" => pattern!("488b05${'} c3 cccccccccccccccc 8b41"),
     "dwLocalPlayerController" => pattern!("488905${'} 8b9e"),
     "dwPlantedC4" => pattern!("488b15${'} 41ffc0"),
     "dwPrediction" => pattern!("488d05${'} c3 cccccccccccccccc 4883ec? 8b0d"),
-    "dwSensitivity" => pattern!("488b05${'} 488b40? f3410f59f4"),
+    "dwSensitivity" => pattern!("488d0d${[8]'} 440f28c1 0f28f3 0f28fa e8"),
     "dwSensitivity_sensitivity" => pattern!("ff50u1 4c8bc6 488d55? 488bcf e8${} 84c0 0f85${} 4c8d45? 8bd3 488bcf e8${} e9${} f30f1006"),
     "dwViewMatrix" => pattern!("488d0d${'} 48c1e006"),
     "dwViewRender" => pattern!("488905${'} 488bc8 4885c0"),
-    "dwWeaponC4" => pattern!("488b15${'} ffc0 8905${} 488bc6 488934ea 488b6c24? c686[5] 80be"),
+    "dwWeaponC4" => pattern!("488b15${'} 488b5c24? ffc0 8905[4] 488bc6 488934ea 488b6c24? c686[5] 80be"),
 };
 
 pub fn offsets(file: PeFile<'_>) -> BTreeMap<&'static str, Rva> {
@@ -48,7 +48,7 @@ pub fn offsets(file: PeFile<'_>) -> BTreeMap<&'static str, Rva> {
                 }
             }
             "dwPrediction" => {
-                map.insert("dwLocalPlayerPawn", rva + 0x148);
+                map.insert("dwLocalPlayerPawn", rva + 0x168);
             }
             _ => {}
         }
