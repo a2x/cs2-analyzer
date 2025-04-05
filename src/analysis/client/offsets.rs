4367fa2b0d2ee4a62e504fbab518a428151b9f9f
a2x <45197573+a2x@users.noreply.github.com>
2025-04-05 12:31:33 +1000

    fix: update patterns

--- src/analysis/client/offsets.rs
+++ src/analysis/client/offsets.rs
@@ -7,7 +7,7 @@ use pelite::pe64::{Pe, PeFile, Rva};
 use phf::phf_map;
 
 static PATTERNS: phf::Map<&'static str, &'static [Atom]> = phf_map! {
-    "dwCSGOInput" => pattern!("488d0d${'} e8${} 488d05${} 48c705[8] 488905${} 488d0d${} 488d05"),
+    "dwCSGOInput" => pattern!("488905${'} 0f57c0 0f1105"),
     "dwEntityList" => pattern!("488935${'} 4885f6"),
     "dwGameEntitySystem" => pattern!("488b1d${'} 48891d"),
     "dwGameEntitySystem_highestEntityIndex" => pattern!("8b81u2?? 8902 488bc2 c3 cccccccc 48895c24? 48896c24"),
@@ -21,7 +21,7 @@ static PATTERNS: phf::Map<&'static str, &'static [Atom]> = phf_map! {
     "dwSensitivity_sensitivity" => pattern!("ff50u1 4c8bc6 488d55? 488bcf e8${} 84c0 0f85${} 4c8d45? 8bd3 488bcf e8${} e9${} f30f1006"),
     "dwViewMatrix" => pattern!("488d0d${'} 48c1e006"),
     "dwViewRender" => pattern!("488905${'} 488bc8 4885c0"),
-    "dwWeaponC4" => pattern!("488b15${'} 488b5c24? ffc0 8905[4] 488bc6 488934ea 488b6c24? c686[5] 80be"),
+    "dwWeaponC4" => pattern!("488b15${'} 488b5c24? ffc0 8905[4] 488bc7"),
 };
 
 pub fn offsets(file: PeFile<'_>) -> BTreeMap<&'static str, Rva> {
@@ -48,7 +48,7 @@ pub fn offsets(file: PeFile<'_>) -> BTreeMap<&'static str, Rva> {
                 }
             }
             "dwPrediction" => {
-                map.insert("dwLocalPlayerPawn", rva + 0x168);
+                map.insert("dwLocalPlayerPawn", rva + 0x180);
             }
             _ => {}
         }
